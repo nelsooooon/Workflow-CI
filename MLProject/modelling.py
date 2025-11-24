d5f16c445095fe14130c49611feab28e969ace11
Nelson <ahlinelson04@gmail.com>
2025-11-24 23:48:27 +0700

    Add CI workflow and refactor modelling script
    Introduces a GitHub Actions CI workflow for running MLflow experiments and saving results. Refactors modelling.py to use command-line arguments for hyperparameters, sets random seed, and improves MLflow logging with input examples and accuracy metric. Updates MLProject to change default max_depth to 37 and fixes formatting in conda.yaml.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,22 +1,34 @@
 import mlflow
 import pandas as pd
+import numpy as np
+import sys
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
-mlflow.set_tracking_uri("http://127.0.0.1:8080/")
-mlflow.set_experiment("Logging Model")
+if __name__ == "__main__":
+    np.random.seed(40)
 
-with mlflow.start_run():
-    mlflow.autolog(log_models=True)
-    
-    df = pd.read_csv("Membangun_model/WA_Fn-UseC_-Telco-Customer-Churn_preprocessing.csv")
-    df.head()
-    
+    df = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn_preprocessing.csv")
+
     x = df.drop(columns=['Churn'])
     y = df['Churn']
 
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-    
-    model_forest = RandomForestClassifier()
-    model_forest.fit(X_train, y_train)
+
+    input_example = X_train[0:5]
+    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
+
+    with mlflow.start_run(run_name=f"elastic_search_{n_estimators}_{max_depth}"):
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        model.fit(X_train, y_train)
+        
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",
+            input_example=input_example
+            )
+        
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
